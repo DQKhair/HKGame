b84657387237fda2c7bfff36cd06bde9559bbe51
DQKhair <jonddemon2307@gmail.com>
2023-11-21 01:14:49 +0700

    UI login

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,9 +1,15 @@
 package main;
 
+import JFrameForm.LoginPlayer;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		new Game();
+            LoginPlayer login = new LoginPlayer();
+            login.pack();
+            login.setLocationRelativeTo(null);
+            login.show();
+//		new Game();
 	}
 
 }
